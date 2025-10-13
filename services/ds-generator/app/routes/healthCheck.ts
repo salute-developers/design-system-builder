5c90d6de2dc43a338f4d136d45c99f07684e3416
Dima Shugaev <shuga2704@gmail.com>
2025-10-13 11:09:40 +0300

    feat(): add startedAt time

--- services/ds-generator/app/routes/healthCheck.ts
+++ services/ds-generator/app/routes/healthCheck.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify';
 
+const timeStart = new Date().toLocaleString();
+
 export const healthCheckRoute = async (server: FastifyInstance) => {
     server.get('/health', async (_, reply) => {
-        reply.code(200).send({ health: 'ok' });
+        reply.code(200).send({ health: 'ok', startedAt: timeStart });
     });
 };
