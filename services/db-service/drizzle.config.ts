53f9ab8bea4133a35466abaaaa60cec455185ce6
Vasiliy <Yeti.or@gmail.com>
2025-10-26 22:18:08 +0300

    build: fix drizzle config

--- services/db-service/drizzle.config.ts
+++ services/db-service/drizzle.config.ts
@@ -7,11 +7,6 @@ export default {
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    // host: process.env.DB_HOST || 'localhost',
-    // port: Number(process.env.DB_PORT) || 5432,
-    // user: process.env.DB_USER || 'postgres',
-    // password: process.env.DB_PASSWORD || 'postgres',
-    // database: process.env.DB_NAME || 'ds_builder',
     url: process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/ds_builder',
   },
 } satisfies Config;
