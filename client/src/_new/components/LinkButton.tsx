015c4289e293655ae2c1c7d56f783adfac68c63c
Neretin Artem <hawktrike@gmail.com>
2025-10-27 17:07:26 +0500

    feat(client): Add `Link,Segment,SelectButton`,`Slider`, `Tooltip` components

---
+++ client/src/_new/components/LinkButton.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from 'react';
+import styled, { CSSObject } from 'styled-components';
+import { textParagraph, textPrimary } from '@salutejs/plasma-themes/tokens/plasma_infra';
+
+import { h6 } from '../utils';
+
+const StyledResetTokeValuesButton = styled.div`
+    cursor: pointer;
+    color: ${textParagraph};
+
+    &:hover {
+        color: ${textPrimary};
+    }
+
+    display: flex;
+    gap: 0.375rem;
+    align-items: center;
+
+    ${h6 as CSSObject};
+`;
+
+interface LinkButtonProps {
+    text: string;
+    contentLeft: ReactNode;
+    onClick?: () => void;
+}
+
+export const LinkButton = (props: LinkButtonProps) => {
+    const { text, contentLeft, onClick, ...rest } = props;
+
+    return (
+        <StyledResetTokeValuesButton onClick={onClick} {...rest}>
+            {contentLeft}
+            <>{text}</>
+        </StyledResetTokeValuesButton>
+    );
+};
