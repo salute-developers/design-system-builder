355dfb4c0d954d898bac89d6a0c392092e1ff75a
Dima Shugaev <shuga2704@gmail.com>
2026-05-25 18:27:23 +0300

    feat(): update PORT config in generator

--- services/generator/app/index.ts
+++ services/generator/app/index.ts
@@ -6,6 +6,8 @@ const fastify = Fastify({ bodyLimit: 10 * 1024 * 1024 });
 import { healthCheckRoute, removeResultRoute, generateAndDownloadRoute, generateAndPublishRoute } from './routes';
 
 (async () => {
+    const port = Number(process.env.PORT) || 3005;
+
     await fastify.register(cors, {
         origin: '*',
     });
@@ -15,5 +17,5 @@ import { healthCheckRoute, removeResultRoute, generateAndDownloadRoute, generate
     await fastify.register(generateAndDownloadRoute);
     await fastify.register(generateAndPublishRoute);
 
-    fastify.listen({ port: 3005, host: '0.0.0.0' });
+    fastify.listen({ port, host: '0.0.0.0' });
 })();
