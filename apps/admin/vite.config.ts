d7ff56c2044e2ac95cdc83e63384822305d2cfe0
Dima Shugaev <shuga2704@gmail.com>
2026-05-26 18:23:53 +0300

    feat(): update vite config at admin

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const base = process.env.BASE_URL || '/';
+
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    allowedHosts: true,
-  },
+    base,
+    plugins: [react()],
+    define: {
+        'process.env': {},
+    },
+    server: {
+        port: 5173,
+        allowedHosts: true,
+    },
 });
