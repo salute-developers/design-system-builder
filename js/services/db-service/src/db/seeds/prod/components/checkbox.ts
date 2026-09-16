0a1986d42198e9a4a25ca4c86b566b91ef11226a
Dima Shugaev <shuga2704@gmail.com>
2026-09-16 11:22:58 +0300

    fix

--- js/services/db-service/src/db/seeds/prod/components/checkbox.ts
+++ js/services/db-service/src/db/seeds/prod/components/checkbox.ts
@@ -4,13 +4,13 @@ import * as schema from '../../../schema';
 export async function seedCheckboxComponent(db: any) {
     const [checkbox] = await db
         .insert(schema.components)
-        .values([{ name: 'CheckBox', description: 'Чекбокс.' }])
+        .values([{ name: 'Checkbox', description: 'Чекбокс.' }])
         .onConflictDoUpdate({
             target: schema.components.name,
             set: { description: sql`excluded.description` },
         })
         .returning();
 
-    console.log(`  components: CheckBox(${checkbox.id})`);
+    console.log(`  components: Checkbox(${checkbox.id})`);
     return checkbox;
 }
--- js/services/db-service/src/db/seeds/prod/components/radiobox.ts
+++ js/services/db-service/src/db/seeds/prod/components/radiobox.ts
@@ -4,13 +4,13 @@ import * as schema from '../../../schema';
 export async function seedRadioboxComponent(db: any) {
     const [radiobox] = await db
         .insert(schema.components)
-        .values([{ name: 'RadioBox', description: 'Переключатель (радиокнопка).' }])
+        .values([{ name: 'Radiobox', description: 'Переключатель (радиокнопка).' }])
         .onConflictDoUpdate({
             target: schema.components.name,
             set: { description: sql`excluded.description` },
         })
         .returning();
 
-    console.log(`  components: RadioBox(${radiobox.id})`);
+    console.log(`  components: Radiobox(${radiobox.id})`);
     return radiobox;
 }
